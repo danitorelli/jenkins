8a25e074db98ef90b01581d1cb6f66c38c80e6e9
danitorelli <65793272+danitorelli@users.noreply.github.com>
2023-06-30 17:39:14 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("build-result").innerText = "Build executado com sucesso!";
+    document.getElementById("test-result").innerText = "Testes passaram!";
+});
